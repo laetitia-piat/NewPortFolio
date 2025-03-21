502d50f35546c2986bf5d085af1da51c5452ecb3
Laetitia <laeptitchat29@gmail.com>
2025-03-21 20:18:24 +0100

    modif menu responsive

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,62 @@
-// sidebar toggle variables
-const menuToggler = document.querySelector('.menu-toggler');
-const sideBar = document.querySelector('.side-bar');
-
 // page navigation variables
-const navItemLinks = document.querySelectorAll('.nav li a');
-const pages = document.querySelectorAll('.page');
-
+const navItemLinks = document.querySelectorAll(".nav li a");
+const pages = document.querySelectorAll(".page");
 
 // variables for filtering
-const filterBtn = document.querySelectorAll('.filter-item');
-const itemCategory = document.querySelectorAll('.item-category');
+const filterBtn = document.querySelectorAll(".filter-item");
+const itemCategory = document.querySelectorAll(".item-category");
 
 // toggling sidebar in mobile
-menuToggler.addEventListener('click', function(){
-  sideBar.classList.toggle('active');
+const menuToggler = document.querySelector(".menu-toggler");
+const sideBar = document.querySelector(".side-bar");
+const menuLinks = document.querySelectorAll(".side-bar .nav a");
+
+menuToggler.addEventListener("click", function () {
+  sideBar.classList.toggle("active");
 });
 
+// Ferme la sidebar lorsqu'on clique sur un lien
+menuLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    sideBar.classList.remove("active"); // Enlève la classe 'active' pour fermer le menu
+  });
+});
 
 // page navigation functionality
 
 for (let i = 0; i < navItemLinks.length; i++) {
   // added onclick event in nav links
-  navItemLinks[i].addEventListener('click', function(){
-
+  navItemLinks[i].addEventListener("click", function () {
     // collected nav links innertext
     const itemLinkText = this.textContent.toLowerCase();
 
-    // defined page and add active class 
+    // defined page and add active class
     for (let i = 0; i < pages.length; i++) {
-
       // defining page condition
       if (pages[i].classList.contains(itemLinkText)) {
         // add active class on current page
-        pages[i].classList.add('active');
+        pages[i].classList.add("active");
         // add active class on clicked nav link
-        navItemLinks[i].classList.add('active');
+        navItemLinks[i].classList.add("active");
       } else {
         // remove active class from other pages
-        pages[i].classList.remove('active');
+        pages[i].classList.remove("active");
         // remove active class from other nav links
-        navItemLinks[i].classList.remove('active');
+        navItemLinks[i].classList.remove("active");
       }
-
     }
-
   });
 }
 
 // added eventListener in filter buttons
 for (let i = 0; i < filterBtn.length; i++) {
-  filterBtn[i].addEventListener('click', function(){
-
+  filterBtn[i].addEventListener("click", function () {
     // remove all active class from filter button
     for (let i = 0; i < filterBtn.length; i++) {
-      filterBtn[i].classList.remove('active');
+      filterBtn[i].classList.remove("active");
     }
     // added active class on filter button clicked
-    this.classList.add('active');
+    this.classList.add("active");
 
     // show item, based on filter button click
     for (let i = 0; i < itemCategory.length; i++) {
@@ -64,13 +64,13 @@ for (let i = 0; i < filterBtn.length; i++) {
       console.log(itemCategoryText);
       switch (this.textContent) {
         case itemCategoryText:
-          itemCategory[i].parentElement.classList.add('active');
+          itemCategory[i].parentElement.classList.add("active");
           break;
-        case 'All':
-          itemCategory[i].parentElement.classList.add('active');
+        case "All":
+          itemCategory[i].parentElement.classList.add("active");
           break;
         default:
-          itemCategory[i].parentElement.classList.remove('active');
+          itemCategory[i].parentElement.classList.remove("active");
       }
     }
   });
